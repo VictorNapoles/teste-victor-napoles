bead7aafcd25d1b71223099345e13b9c96598194
VictorNapoles <victor.avansys@gmail.com>
2018-04-13 13:33:23 -0300

    Update HelloController.java
    Ajuste na requisição inical.

--- src/main/java/br/com/fcamara/testevictornapoles/controller/HelloController.java
+++ src/main/java/br/com/fcamara/testevictornapoles/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Desafio Técnico Saraiva - Analista JAVA - Victor de Nápoles dos Santos";
     }
 }
